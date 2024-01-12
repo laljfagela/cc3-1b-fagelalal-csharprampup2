507a83679fb15b36c67ed8ba964bc8e649cfb45c
Lucci Fagela <145944229+laljfagela@users.noreply.github.com>
2024-01-13 07:51:12 +0800

    Update studentsdraft.cs

--- studentsdraft.cs
+++ studentsdraft.cs
@@ -1,1 +1,32 @@
 
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       string[] characters ={"Vox Akuma", "Luca Kaneshiro", "Mysta Rias", "Ike Eveland", "Shu Yamino"}
+       string[] ability ={"Voice Demon", "Mafia", "Fox", "Time Traveler", "Sorcerer"}
+       string[] equipments = {"Blue-Diamond Pen", "Purple Flames", "Pomu Leaf", "Dragoon", "Purple Flamed Fire"}
+       
+      var Story = $""" In the Royal Flash Kingdom, 5 lads possessed different magical powers and capabilities. +
+      {characters[0]},  the {ability[0]}, has the power to influence others with his voice.+
+      {characters[1]}, the mean and evil {ability[1]} boss from the past. +
+      {characters[2]}, the genius detective, is also a {ability[2]} in nature. +
+      {characters[3]}, is a novelist who is a {ability[3]} using his {equipments[0]}. +
+      And finally, {characters[4]}, a {ability[4]} with the ability to control his victim’s + 
+      minds through his magical {equipments[1]}+.
+      They are heroes. As they ventured through Diamond City Lights, +
+      they were met with various elements that affected their powers gradually,+
+      the {equipments[2]}, remnants of Pomu Fairies who died because of a so-called angel,+
+      Enna Alouette. Enna Alouette used the Pomu Leaf to lure the five lads to her. +
+      However, before they arrive in Enna’s kingdom, they met a flaming purple {equipments[3]},+
+      Selen Tatsuki. Selen attacked the five lads,+
+      and they fought their hardest to defend and attack. +
+      But, Selen’s{equipments[4]} was so powerful, and Mysta sacrificed himself. +
+      Taking the attack, to help save his friends. Mysta died. """ 
+    }
+} 
+
+
+// still has python element lol 
